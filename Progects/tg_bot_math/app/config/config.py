2cf72ad920d0593d78697fea91a08987f278a995
Mihajlov Dmitry <dimonmihajlov10@gmail.com>
2024-04-01 15:37:06 +0500

    Delete Progects/tg_bot_math directory

--- Progects/tg_bot_math/app/config/config.py
+++
@@ -1,5 +0,0 @@
-from environs import Env
-
-env = Env()
-env.read_env()
-TOKEN_API = env.str("TOKEN_API")
--- Progects/tg_bot_math/app/utils/text.py
+++
@@ -1,6 +0,0 @@
-text_data = {
-    'main': 'lox',
-    'catalog': 'catalog',
-    'output_elements': 'output_elements',
-    'send_data': 'send_data'
-}
